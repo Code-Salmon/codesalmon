571fde0d17b34a6c20fa4970e278ab90d8212a19
Anne Salim <annebaumhower@gmail.com>
2025-05-23 09:49:50 -0500

    chalk added

--- drift.ts
+++ drift.ts
@@ -1,32 +1,34 @@
 import { diff, Diff } from 'deep-diff';
+import chalk from 'chalk';
 
 type JSONObj = Record<string, unknown>;
 
 export function compareAPIs(initial: JSONObj, current: JSONObj): void {
-    const drift: Diff<JSONObj, JSONObj>[] | undefined = diff(initial, current);
-    if (!drift){
-        console.log("no drift detected!");
-        return
-    }
-    console.log('Drift detected: \n');
+  const drift: Diff<JSONObj, JSONObj>[] | undefined = diff(initial, current);
+  if (!drift) {
+    console.log(chalk.bold.bgWhite.greenBright('✅ No drift detected!'));
+    return;
+  }
+  console.log(chalk.yellow.bold('\n⚠️ Drift detected:\n'));
 
-    
-    drift.forEach((update) => {
-        const path = change.path?.join('.') || '(root)';
-        switch (update.kind) {
-            case 'E':
-                console.log(`Change found: ${path}`);
-                console.log(`From: ${JSON.stringify(update.lhs)}`);
-                console.log(`To: ${JSON.stringify(update.rhs)}`);
-                break;
-            case 'N':
-                console.log(`Addition was made: ${path}`);
-                console.log(`Value: ${JSON.stringify(update.rhs)}\n`);
-                break;
-            case 'D':
-                console.log(chalk.red(`Removed: ${path}`));
-                console.log(`Old value: ${JSON.stringify(update.lhs)}\n`);
-                break;   
-        }
-    });
-}        
+  drift.forEach((update) => {
+    const path = change.path?.join('.') || '(root)';
+    switch (update.kind) {
+      case 'E':
+        console.log(chalk.blueBright(`✏️  Change at: ${path}`));
+        console.log(`From: ${JSON.stringify(update.lhs)}`);
+        console.log(`To: ${JSON.stringify(update.rhs)}`);
+        break;
+      case 'N':
+        console.log(chalk.greenBright(`➕ Addition at: ${path}`));
+        console.log(`Value: ${JSON.stringify(update.rhs)}\n`);
+        break;
+      case 'D':
+        console.log(chalk.redBright(`❌ Removed at: ${path}`));
+        console.log(`Old value: ${JSON.stringify(update.lhs)}\n`);
+        break;
+      default:
+        console.log(chalk.gray(`Unhandled change type at ${path}`));
+    }
+  });
+}
